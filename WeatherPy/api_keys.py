67d936ff2f57c5c243e8bdcedfcedcc8a9df6154
merepbj <meredithjohnson76@gmail.com>
2021-01-14 22:15:52 -0500

    to remove api keys

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "e9c9b5e306770e4fac5d2d55d2761a7c"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyCc1apLe992yqAzdTo8nLiTEJrxosIUB8Q​"
+g_key = ""
