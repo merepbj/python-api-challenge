d27e7b4e396df064b300d487d57f957e4a49a9eb
merepbj <meredithjohnson76@gmail.com>
2021-01-18 19:24:59 -0500

    take out API keys and push final analysis

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "e9c9b5e306770e4fac5d2d55d2761a7c"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyCgZ6UfWxwoslJuoe5Vpy6SZNbCZ-L6v4Y"
+g_key = ""
